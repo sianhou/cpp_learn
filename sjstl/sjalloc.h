6c8020aaa72259c6d109b79bb20c65559b8d958f
sian hou <sianhou1987@gmail.com>
2023-05-13 12:45:47 +0800

    __default_alloc_template

--- sjstl/sjalloc.h
+++ sjstl/sjalloc.h
@@ -15,8 +15,8 @@ namespace sj {
 template<class T>
 inline T *_allocte(ptrdiff_t size, T *) {
     std::set_new_handler(0);
-    T *tmp = (T *) (::operator new((size_t) (size * sizeof(T))));
-    if (tmp == 0) {
+    T *tmp = (T *)(::operator new((size_t)(size*sizeof(T))));
+    if (tmp==nullptr) {
         std::cerr << "out of memory" << std::endl;
         exit(EXIT_FAILURE);
     }
@@ -46,48 +46,48 @@ inline void _destroy(T *ptr) {
 template<typename T>
 class allocator {
  public:
-  using value_type = T;
-  using pointer = T *;
-  using const_pointer = const T *;
-  using reference = T &;
-  using const_reference = const T &;
-  using size_type = size_t;
-  using difference_type = ptrdiff_t;
+    using value_type = T;
+    using pointer = T *;
+    using const_pointer = const T *;
+    using reference = T &;
+    using const_reference = const T &;
+    using size_type = size_t;
+    using difference_type = ptrdiff_t;
 
 // rebind alloctor of type U
-  template<class U>
-  struct rebind {
-    using other = allocator<U>;
-  };
+    template<class U>
+    struct rebind {
+        using other = allocator<U>;
+    };
 
-  // hint used for locality
-  pointer allocate(size_type n, const void *hint = 0) {
-      return _allocte((difference_type) n, (pointer) 0);
-  }
+    // hint used for locality
+    pointer allocate(size_type n, const void *hint = 0) {
+        return _allocte((difference_type)n, (pointer)0);
+    }
 
-  void deallocate(pointer p, size_type n) {
-      _deallocate(p);
-  }
+    void deallocate(pointer p, size_type n) {
+        _deallocate(p);
+    }
 
-  void construct(pointer p, const T &value) {
-      _construct(p, value);
-  }
+    void construct(pointer p, const T &value) {
+        _construct(p, value);
+    }
 
-  void destroy(pointer p) {
-      _destroy(p);
-  }
+    void destroy(pointer p) {
+        _destroy(p);
+    }
 
-  pointer address(reference x) {
-      return (pointer) &x;
-  }
+    pointer address(reference x) {
+        return (pointer)&x;
+    }
 
-  const_pointer const_address(const_reference x) {
-      return (const_pointer) &x;
-  }
+    const_pointer const_address(const_reference x) {
+        return (const_pointer)&x;
+    }
 
-  size_type max_size() const {
-      return size_type(UINT_MAX / (sizeof(T)));
-  }
+    size_type max_size() const {
+        return size_type(UINT_MAX/(sizeof(T)));
+    }
 
 };
 
@@ -105,42 +105,43 @@ class allocator {
 template<int inst>
 class __malloc_alloc_template {
  private:
-  //oom: out of memory
-  static void *oom_malloc(size_t);
-  static void *oom_realloc(void *, size_t);
-  static void (*__malloc_alloc_oom_handler)();
+    //oom: out of memory
+    static void *oom_malloc(size_t);
+    static void *oom_realloc(void *, size_t);
+    static void (*__malloc_alloc_oom_handler)();
 
  public:
-  static void *allocate(size_t n) {
-      void *result;
-      if (0 == (result = malloc(n))) { // use malloc directly in the first class allocator
-          result = oom_malloc(n);   // use oom when malloc fail
-      }
-      return result;
-  }
+    static void *allocate(size_t n) {
+        void *result;
+        if (nullptr==(result = malloc(n))) { // use malloc directly in the first class allocator
+            result = oom_malloc(n);   // use oom when malloc fail
+        }
+        return result;
+    }
 
-  static void *deallocate(void *p, size_t) {
-      free(p);
-  }
+    static void *deallocate(void *p, size_t) {
+        free(p);
+        return p;
+    }
 
-  static void *reallocate(void *p, size_t, size_t new_size) {
-      void *result;
-      if (0 == (result = realloc(p, new_size))) {
-          result = oom_realloc(p, new_size);
-      }
-      return result;
-  }
+    static void *reallocate(void *p, size_t, size_t new_size) {
+        void *result;
+        if (nullptr==(result = realloc(p, new_size))) {
+            result = oom_realloc(p, new_size);
+        }
+        return result;
+    }
 
-  static void (*set_malloc_handler(void (*f)()))() {
-      void (*old)() = __malloc_alloc_oom_handler;
-      __malloc_alloc_oom_handler = f;
-      return old;
-  }
+    static void (*set_malloc_handler(void (*f)()))() {
+        void (*old)() = __malloc_alloc_oom_handler;
+        __malloc_alloc_oom_handler = f;
+        return old;
+    }
 };
 
 // default 0, you must set it
 template<int inst>
-void (*__malloc_alloc_template<inst>::__malloc_alloc_oom_handler)() = 0;
+void (*__malloc_alloc_template<inst>::__malloc_alloc_oom_handler)() = nullptr;
 
 template<int inst>
 void *__malloc_alloc_template<inst>::oom_malloc(size_t n) {
@@ -149,11 +150,11 @@ void *__malloc_alloc_template<inst>::oom_malloc(size_t n) {
 
     do {
         my_malloc_handler = __malloc_alloc_oom_handler;
-        if (0 == my_malloc_handler) {
+        if (nullptr==my_malloc_handler) {
             __THROW_BAD_ALLOC;
         }
         (*my_malloc_handler)(); // 调用处理函数，试图释放内存
-        if (0 != (result = malloc(n))) {
+        if (0!=(result = malloc(n))) {
             return result;
         }
     } while (1);
@@ -166,17 +167,186 @@ void *__malloc_alloc_template<inst>::oom_realloc(void *p, size_t n) {
 
     do {
         my_malloc_handler = __malloc_alloc_oom_handler;
-        if (0 == my_malloc_handler) {
+        if (0==my_malloc_handler) {
             __THROW_BAD_ALLOC;
         }
         (*my_malloc_handler)(); // 调用处理函数，试图释放内存
-        if (0 != (result = realloc(p, n))) {
+        if (0!=(result = realloc(p, n))) {
             return result;
         }
     } while (1);
 }
 
 typedef __malloc_alloc_template<0> malloc_alloc;
+
+// __default_alloc_template
+enum { __ALIGN = 8 };
+enum { __MAX_BYTES = 128 };
+enum { __NFREELISTS = __MAX_BYTES/__ALIGN };
+
+template<bool threads, int inst>
+class __default_alloc_template {
+ private:
+    // round up to 8 times
+    static size_t ROUND_UP(size_t bytes) {
+        return ((bytes + __ALIGN - 1) & ~(__ALIGN - 1));
+    }
+ private:
+    union obj {
+        union obj *free_list_link;
+        char client_data[1];
+    };
+ private:
+    // 16 free-lists
+    static obj *volatile free_list[__NFREELISTS];
+    // calculate the index
+    static size_t FREELIST_INDEX(size_t bytes) {
+        return ((bytes + __ALIGN - 1)/__ALIGN - 1);
+    }
+
+    // return a n size obj, may add a n block to free list
+    static void *refill(size_t n) {
+        int nobjs = 20;
+
+        // use chunk_alloc
+        char *chunk = chunk_alloc(n, nobjs);
+        obj *volatile *my_free_list;
+        obj *result;
+        obj *current_obj, *next_obj;
+
+        if (1==nobjs) return (chunk);
+
+        my_free_list = free_list + FREELIST_INDEX(n);
+        result = (obj *)chunk;
+        *my_free_list = next_obj = (obj *)(chunk + n);
+        for (int i = 1;; ++i) {
+            current_obj = next_obj;
+            next_obj = (obj *)((char *)next_obj + n);
+            if (nobjs - 1==i) {
+                current_obj->free_list_link = nullptr;
+                break;
+            } else {
+                current_obj->free_list_link = next_obj;
+            }
+        }
+        return result;
+    }
+
+    // allocate a big space which contains nobjs chunk
+    // if nobjs is inavailable, nobjs may decrease
+    // size must be times of 8
+    static char *chunk_alloc(size_t size, int &nobjs) {
+        char *result;
+        size_t total_bytes = size*nobjs;
+        size_t bytes_left = end_free - start_free; // left space in memory pool
+
+        if (bytes_left >= total_bytes) {
+            result = start_free;
+            start_free += total_bytes;
+            return result;
+        } else if (bytes_left >= size) {
+            nobjs = bytes_left/size;
+            total_bytes = size* nobjs;
+            result = start_free;
+            start_free += total_bytes;
+            return result;
+        } else {
+            size_t bytes_to_get = 2*total_bytes + ROUND_UP(heap_size >> 4);
+
+            // use the left bytes in memory pool
+            if (bytes_left > 0) {
+                obj *volatile *my_free_list = free_list + FREELIST_INDEX(bytes_left);
+                ((obj *)start_free)->free_list_link = *my_free_list;
+                *my_free_list = (obj *)start_free;
+            }
+
+            // allocate heap
+            start_free = (char *)malloc(bytes_to_get);
+            if (nullptr==start_free) {
+                // malloc fail
+                int i;
+                obj *volatile *my_free_list, *p;
+                for (int i = size; i < __MAX_BYTES; i += __ALIGN) {
+                    my_free_list = free_list + FREELIST_INDEX(i);
+                    p = *my_free_list;
+                    if (nullptr!=p) {
+                        *my_free_list = p->free_list_link;
+                        start_free = (char *)p;
+                        end_free=start_free+i;
+                        // use every small blocks
+                        return (chunk_alloc(size,nobjs));
+                    }
+                }
+            }
+            end_free = nullptr;
+            start_free = (char *)malloc_alloc::allocate(bytes_to_get);
+            heap_size += bytes_to_get;
+            return chunk_alloc(size,nobjs);
+        }
+    }
+
+    // chunk allocation state
+    static char *start_free;
+    static char *end_free;
+    static size_t heap_size;
+
+ public:
+    static void *allocate(size_t n) {
+        // n must be > 0
+        obj *volatile *my_free_list;
+        obj *result;
+
+        // use the first class allocate if n > 128
+        if (n > (size_t)__MAX_BYTES) {
+            return (malloc_alloc::allocate(n));
+        }
+
+        // find a proper list in free lists
+        my_free_list = free_list + FREELIST_INDEX(n);
+        result = *my_free_list;
+        if (result==0) {
+            // don't find available free list
+            void *r = refill(ROUND_UP(n));
+            return r;
+        }
+
+        // set free list
+        *my_free_list = result->free_list_link;
+        return result;
+    }
+
+    static void deallocate(void *p, size_t n) {
+        obj *q = (obj *)p;
+        obj *volatile *my_free_list;
+
+        // if n > 128
+        if (n > (size_t)__MAX_BYTES) {
+            malloc_alloc::deallocate(p, n);
+            return;
+        }
+
+        // find index
+        my_free_list = free_list + FREELIST_INDEX(n);
+        q->free_list_link = *my_free_list;
+        *my_free_list = q;
+    }
+
+    static void *reallocate(void *p, size_t old_size, size_t new_size) {}
+};
+
+template<bool threads, int inst>
+char *__default_alloc_template<threads, inst>::start_free = 0;
+
+template<bool threads, int inst>
+char *__default_alloc_template<threads, inst>::end_free = 0;
+
+template<bool threads, int inst>
+size_t __default_alloc_template<threads, inst>::heap_size = 0;
+
+template<bool threads, int inst>
+typename __default_alloc_template<threads, inst>::obj *volatile
+    __default_alloc_template<threads, inst>::free_list[__NFREELISTS] =
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,};
 }
 
 #endif //LEARN_CXX_SJSTL_SJALLOC_H_
--- sjstl/test_sjalloc.cpp
+++ sjstl/test_sjalloc.cpp
@@ -7,30 +7,31 @@
 int main() {
 
     // test alloc
-    int ia[5] = {0, 1, 2, 3, 4};
+//    int ia[5] = {0, 1, 2, 3, 4};
     size_t i;
-
-    std::vector<int, sj::allocator<int>> iv(ia, ia + 5);
-    for (i = 0; i < iv.size(); ++i) {
-        std::cout << iv[i] << " ";
-    }
-    std::cout << std::endl;
+//
+//    std::vector<int, sj::allocator<int>> iv(ia, ia + 5);
+//    for (i = 0; i < iv.size(); ++i) {
+//        std::cout << iv[i] << " ";
+//    }
+//    std::cout << std::endl;
 
     // test malloc_alloc
-    sj::malloc_alloc malloc_alloc;
-
     auto handler = []() {
-      std::cout << "allocate failed!" << std::endl;
-      exit(EXIT_FAILURE);
+        std::cout << "allocate failed!" << std::endl;
+        exit(EXIT_FAILURE);
     };
-    malloc_alloc.set_malloc_handler(handler);
+    sj::malloc_alloc::set_malloc_handler(handler);
 
-    i = 1024;
-    void *p = malloc_alloc.allocate(i * sizeof(int));
-    do {
-        i *= 2;
-        std::cout << "allocate " << i * sizeof(int) / (8 * 1024.0f * 1024.0f * 1024.0f) << "GBs!" << std::endl;
-        p = malloc_alloc.reallocate(p, i / 2, i);
-    } while (i);
+    i = 512*512*512;
+    void *p = sj::malloc_alloc::allocate(i*sizeof(int));
+    i *= 2;
+    p = sj::malloc_alloc::reallocate(p, i/2, i*sizeof(int));
+    sj::malloc_alloc::deallocate(p, i*sizeof(int));
 
+    // test
+    p = sj::__default_alloc_template<true, 0>::allocate(16);
+    void *q = sj::__default_alloc_template<true, 0>::allocate(16);
+    sj::__default_alloc_template<true, 0>::deallocate(p, 16);
+    sj::__default_alloc_template<true, 0>::deallocate(q, 16);
 }
