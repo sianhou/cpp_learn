f0bddb2691674b3b8c155eee638f1d7fc068fa14
housian <sianhou1987@gmail.com>
2023-05-20 22:33:03 +0800

    iterator & construct

--- sjstl/sjconstruct.h
+++ sjstl/sjconstruct.h
@@ -6,37 +6,154 @@
 #define LEARN_CPP_SJSTL_SJCONSTRUCT_H_
 
 #include "new"
+#include "sjiterator.h"
 
 namespace sj {
 template<class T1, class T2>
 inline void construct(T1 *p, const T2 &value) {
-    new(p) T1(value);
+  new(p) T1(value);
 }
 
 template<class T>
 inline void destroy(T *pointer) {
-    pointer->~T();
-}
-
-template<class ForwardIterator>
-inline void destroy(ForwardIterator first, ForwardIterator last) {
-
+  pointer->~T();
 }
 
 template<class ForwardIterator>
 inline void
-__destroy_aux(ForwardIterator first, ForwardIterator last, std::false_type) {
-    for (; first < last; ++first) {
-        destroy(&*first);
-    }
+__destroy_aux(ForwardIterator first, ForwardIterator last, __false_type) {
+  for (; first < last; ++first) {
+    destroy(&*first);
+  }
 }
 
 template<class ForwardIterator>
 inline void
-__destroy_aux(ForwardIterator first, ForwardIterator last, std::true_type) {}
+__destroy_aux(ForwardIterator first, ForwardIterator last, __true_type) {}
+
+template<class ForwardIterator, class T>
+inline void __destroy(ForwardIterator first, ForwardIterator last, T *) {
+  typedef typename __type_traits<T>::has_trivial_destructor trivial_destructor;
+  __destroy_aux(first, last, trivial_destructor());
+}
+
+template<class ForwardIterator>
+inline void destory(ForwardIterator first, ForwardIterator last) {
+  __destroy(first, last, value_type(first));
+}
 
 inline void destroy(char *, char *) {}
 inline void destroy(wchar_t *, wchar_t *) {}
+
+// uninitialized_copy
+template<class InputIterator, class ForwardIterator>
+inline ForwardIterator
+__uninitialized_copy_aux(InputIterator first, InputIterator last, ForwardIterator result, __true_type) {
+  //TODO(HOUSIAN): use std now, change to personal stl later
+  return std::copy(first, last, result);
+}
+
+template<class InputIterator, class ForwardIterator>
+inline ForwardIterator
+__uninitialized_copy_aux(InputIterator first, InputIterator last, ForwardIterator result, __false_type) {
+  ForwardIterator cur = result;
+  while (first != last) {
+    construct(&*cur, *first);
+    first++;
+    cur++;
+  }
+  return cur;
+}
+
+template<class InputIterator, class ForwardIterator, class T>
+inline ForwardIterator
+__uninitialized_copy(InputIterator first, InputIterator last, ForwardIterator result, T *) {
+  typedef typename __type_traits<InputIterator>::is_POD_type is_POD;
+  return __uninitialized_copy_aux(first, last, result, is_POD());
+}
+
+template<class InputIterator, class ForwardIterator>
+inline ForwardIterator
+uninitialized_copy(InputIterator first, InputIterator last, ForwardIterator result) {
+  return __uninitialized_copy_aux(first, last, result, value_type(first));
+}
+
+template<>
+inline char *uninitialized_copy(const char *first, const char *last, char *result) {
+  memmove(result, first, last - first);
+  return result + (last - first);
+}
+
+template<>
+inline wchar_t *uninitialized_copy(const wchar_t *first, const wchar_t *last, wchar_t *result) {
+  memmove(result, first, sizeof(wchar_t) * (last - first));
+  return result + (last - first);
+}
+
+// uninitialized_fill
+template<class ForwardIterator, class T>
+inline void
+__uninitialized_fill_aux(ForwardIterator first, ForwardIterator last, const T &x, __true_type) {
+  //TODO(HOUSIAN): use std now, change to personal stl later
+  std::fill(first, last, x);
+}
+
+template<class ForwardIterator, class T>
+inline void
+__uninitialized_fill_aux(ForwardIterator first, ForwardIterator last, const T &x, __false_type) {
+  ForwardIterator cur = first;
+  for (; cur != last; ++cur) {
+    construct(&*cur, x);
+  }
+}
+
+template<class ForwardIterator, class T, class T1>
+inline void __uninitialized_fill(ForwardIterator first, ForwardIterator last, const T &x, T1 *) {
+  typedef typename __type_traits<T1>::is_POD_type is_POD;
+  __uninitialized_copy_aux(first, last, x, is_POD());
+}
+
+template<class ForwardIterator, class T>
+inline void uninitialized_fill(ForwardIterator first, ForwardIterator last, const T &x) {
+  __uninitialized_fill(first, last, x, value_type(first));
+}
+
+// uninitialized_fill_n
+
+template<class OutputIterator, class Size, class T>
+OutputIterator fill_n(OutputIterator first, Size n, const T &value) {
+  for (; n > 0; --n, ++first) {
+    *first = value;
+  }
+  return first;
+}
+
+template<class ForwardIterator, class Size, class T>
+ForwardIterator
+__uninitialzied_fill_n_aux(ForwardIterator first, Size n, const T &x, __false_type) {
+  ForwardIterator cur = first;
+  for (; n > 0; --n, ++cur) {
+    construct(&*cur, x);
+  }
+  return cur;
+}
+
+template<class ForwardIterator, class Size, class T>
+ForwardIterator
+__uninitialzied_fill_n_aux(ForwardIterator first, Size n, const T &x, __true_type) {
+  return fill_n(first, n, x);
+}
+
+template<class ForwardIterator, class Size, class T, class T1>
+inline ForwardIterator __uninitialized_fill_n(ForwardIterator first, Size n, const T &x, T1 *) {
+  typedef typename __type_traits<T1>::is_POD_type is_POD;
+  return __uninitialzied_fill_n_aux(first, n, x, is_POD());
+}
+
+template<class ForwardIterator, class Size, class T>
+inline ForwardIterator uninitialized_fill_n(ForwardIterator first, Size n, const T &x) {
+  return __uninitialized_fill_n(first, n, x, value_type(first));
+}
 
 }
 
---
+++ sjstl/sjiterator.h
@@ -0,0 +1,253 @@
+//
+// Created by 侯思安 on 2023/5/20.
+//
+
+#ifndef LEARN_CPP_SJSTL_SJITERATOR_H_
+#define LEARN_CPP_SJSTL_SJITERATOR_H_
+
+namespace sj {
+struct input_iterator_tag {};
+struct output_iterator_tag {};
+struct forward_iterator_tag : public input_iterator_tag {};
+struct bidirectional_iterator_tag : public forward_iterator_tag {};
+struct random_access_iterator_tag : public bidirectional_iterator_tag {};
+
+template<class Category, class T, class Distance = ptrdiff_t, class Pointer = T *, class Reference = T &>
+struct iterator {
+  typedef Category iterator_category;
+  typedef T value_type;
+  typedef Distance difference_type;
+  typedef Pointer pointer;
+  typedef Reference reference;
+};
+
+template<class Iterator>
+struct iterator_traits {
+  typedef typename Iterator::iterator_category iterator_category;
+  typedef typename Iterator::value_type value_type;
+  typedef typename Iterator::difference_type difference_type;
+  typedef typename Iterator::pointer pointer;
+  typedef typename Iterator::reference reference;
+};
+
+template<class T>
+struct iterator_traits<T *> {
+  typedef random_access_iterator_tag iterator_category;
+  typedef T value_type;
+  typedef ptrdiff_t difference_type;
+  typedef T *pointer;
+  typedef T &reference;
+};
+
+template<class T>
+struct iterator_traits<const T *> {
+  typedef random_access_iterator_tag iterator_category;
+  typedef T value_type;
+  typedef ptrdiff_t difference_type;
+  typedef const T *pointer;
+  typedef const T &reference;
+};
+
+template<class Iterator>
+inline typename iterator_traits<Iterator>::iterator_category
+iterator_category(const Iterator &) {
+  typedef typename iterator_traits<Iterator>::iterator_category category;
+  return category();
+}
+
+template<class Iterator>
+inline typename iterator_traits<Iterator>::difference_type *
+distance_type(const Iterator &) {
+  return static_cast<typename iterator_traits<Iterator>::difference_type *>(0);
+}
+
+template<class Iterator>
+inline typename iterator_traits<Iterator>::value_type *
+value_type(const Iterator &) {
+  return static_cast<typename iterator_traits<Iterator>::value_type *>(0);
+}
+
+template<class InputIterator>
+inline typename iterator_traits<InputIterator>::difference_type
+__distance(InputIterator first, InputIterator last, input_iterator_tag) {
+  typename iterator_traits<InputIterator>::difference_type n = 0;
+  while (first != last) {
+    ++n;
+    ++first;
+  }
+  return n;
+}
+
+template<class RandomAccessIterator>
+inline typename iterator_traits<RandomAccessIterator>::difference_type
+__distance(RandomAccessIterator first, RandomAccessIterator last, random_access_iterator_tag) {
+  return last - first;
+}
+
+template<class InputIterator>
+inline typename iterator_traits<InputIterator>::difference_type
+distance(InputIterator first, InputIterator last) {
+  typedef typename iterator_traits<InputIterator>::iterator_category category;
+  return __distance(first, last, category());
+}
+
+template<class InputIterator, class Distance>
+inline void __advance(InputIterator &i, Distance n, input_iterator_tag) {
+  while (n--) ++i;
+}
+
+template<class BidirectionalIterator, class Distance>
+inline void __advance(BidirectionalIterator &i, Distance n, bidirectional_iterator_tag) {
+  if (n >= 0) {
+    while (n--) ++i;
+  } else {
+    while (n++) --i;
+  }
+}
+
+template<class RandomAccessIterator, class Distance>
+inline void __advance(RandomAccessIterator &i, Distance n, random_access_iterator_tag) {
+  i += n;
+}
+
+template<class InputIterator, class Distance>
+inline void Advance(InputIterator &i, Distance n) {
+  __distance(i, n, iterator_category(i));
+}
+
+struct __true_type {};
+struct __false_type {};
+
+template<class type>
+struct __type_traits {
+  typedef __true_type this_dummy_member_must_be_first;
+  typedef __false_type has_trivial_default_constructor;
+  typedef __false_type has_trivial_copy_constructor;
+  typedef __false_type has_trivial_assignment_constructor;
+  typedef __false_type has_trivial_destructor;
+  typedef __false_type is_POD_type;
+};
+
+template<>
+struct __type_traits<char> {
+  typedef __true_type this_dummy_member_must_be_first;
+  typedef __true_type has_trivial_default_constructor;
+  typedef __true_type has_trivial_copy_constructor;
+  typedef __true_type has_trivial_assignment_constructor;
+  typedef __true_type has_trivial_destructor;
+  typedef __true_type is_POD_type;
+};
+
+template<>
+struct __type_traits<signed char> {
+  typedef __true_type this_dummy_member_must_be_first;
+  typedef __true_type has_trivial_default_constructor;
+  typedef __true_type has_trivial_copy_constructor;
+  typedef __true_type has_trivial_assignment_constructor;
+  typedef __true_type has_trivial_destructor;
+  typedef __true_type is_POD_type;
+};
+
+template<>
+struct __type_traits<unsigned char> {
+  typedef __true_type this_dummy_member_must_be_first;
+  typedef __true_type has_trivial_default_constructor;
+  typedef __true_type has_trivial_copy_constructor;
+  typedef __true_type has_trivial_assignment_constructor;
+  typedef __true_type has_trivial_destructor;
+  typedef __true_type is_POD_type;
+};
+
+template<>
+struct __type_traits<short> {
+  typedef __true_type this_dummy_member_must_be_first;
+  typedef __true_type has_trivial_default_constructor;
+  typedef __true_type has_trivial_copy_constructor;
+  typedef __true_type has_trivial_assignment_constructor;
+  typedef __true_type has_trivial_destructor;
+  typedef __true_type is_POD_type;
+};
+
+template<>
+struct __type_traits<unsigned short> {
+  typedef __true_type this_dummy_member_must_be_first;
+  typedef __true_type has_trivial_default_constructor;
+  typedef __true_type has_trivial_copy_constructor;
+  typedef __true_type has_trivial_assignment_constructor;
+  typedef __true_type has_trivial_destructor;
+  typedef __true_type is_POD_type;
+};
+
+template<>
+struct __type_traits<int> {
+  typedef __true_type this_dummy_member_must_be_first;
+  typedef __true_type has_trivial_default_constructor;
+  typedef __true_type has_trivial_copy_constructor;
+  typedef __true_type has_trivial_assignment_constructor;
+  typedef __true_type has_trivial_destructor;
+  typedef __true_type is_POD_type;
+};
+
+template<>
+struct __type_traits<unsigned int> {
+  typedef __true_type this_dummy_member_must_be_first;
+  typedef __true_type has_trivial_default_constructor;
+  typedef __true_type has_trivial_copy_constructor;
+  typedef __true_type has_trivial_assignment_constructor;
+  typedef __true_type has_trivial_destructor;
+  typedef __true_type is_POD_type;
+};
+
+template<>
+struct __type_traits<long> {
+  typedef __true_type this_dummy_member_must_be_first;
+  typedef __true_type has_trivial_default_constructor;
+  typedef __true_type has_trivial_copy_constructor;
+  typedef __true_type has_trivial_assignment_constructor;
+  typedef __true_type has_trivial_destructor;
+  typedef __true_type is_POD_type;
+};
+
+template<>
+struct __type_traits<unsigned long> {
+  typedef __true_type this_dummy_member_must_be_first;
+  typedef __true_type has_trivial_default_constructor;
+  typedef __true_type has_trivial_copy_constructor;
+  typedef __true_type has_trivial_assignment_constructor;
+  typedef __true_type has_trivial_destructor;
+  typedef __true_type is_POD_type;
+};
+
+template<>
+struct __type_traits<float> {
+  typedef __true_type this_dummy_member_must_be_first;
+  typedef __true_type has_trivial_default_constructor;
+  typedef __true_type has_trivial_copy_constructor;
+  typedef __true_type has_trivial_assignment_constructor;
+  typedef __true_type has_trivial_destructor;
+  typedef __true_type is_POD_type;
+};
+
+template<>
+struct __type_traits<double> {
+  typedef __true_type this_dummy_member_must_be_first;
+  typedef __true_type has_trivial_default_constructor;
+  typedef __true_type has_trivial_copy_constructor;
+  typedef __true_type has_trivial_assignment_constructor;
+  typedef __true_type has_trivial_destructor;
+  typedef __true_type is_POD_type;
+};
+
+template<class T>
+struct __type_traits<T *> {
+  typedef __true_type this_dummy_member_must_be_first;
+  typedef __true_type has_trivial_default_constructor;
+  typedef __true_type has_trivial_copy_constructor;
+  typedef __true_type has_trivial_assignment_constructor;
+  typedef __true_type has_trivial_destructor;
+  typedef __true_type is_POD_type;
+};
+
+}
+
+#endif //LEARN_CPP_SJSTL_SJITERATOR_H_
